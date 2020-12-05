7ea9e6a3cf5392215f9f52f9b3dd4701887c9b71
Squalm <43246103+Squalm@users.noreply.github.com>
2020-12-05 10:16:16 +0000

    train_neural outline

---
+++ Neural.jl
@@ -0,0 +1,24 @@
+# I'm going to build my own neural net from scratch because why not
+
+"""
+    train_neural(training_data, data_set; iterations, seed, verbose, graphic)
+
+```
+iterations = 100
+seed = 1337
+verbose = true
+graphic = true
+```
+"""
+function train_neural(
+    training_data,
+    layers;
+    iteratations = 100,
+    seed = 1337,
+    verbose = true,
+    graphic = true,
+)
+
+
+
+end # function
